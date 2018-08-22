fcf64538a94e64db66249aab7758726fb3b7c410
Piyush Naveen Bansal <bansalpiyush456@gmail.com>
2018-08-22 18:41:34 +0530

    Added projects template page

---
+++ js/project-page.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	$('#footer').load('footer.html');
+    $('.sidenav').sidenav();
+    $('#floor-plans .carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+    $('#project-images .carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+    $('.modal').modal();
+});
+
+var floor_timer;
+var project_timer;
+
+var checkScrollBars = function(){
+    var b = $('body');
+    var normalw = 0;
+    var scrollw = 0;
+    if(b.prop('scrollHeight')>b.height()){
+        normalw = window.innerWidth;
+        scrollw = normalw - b.width();
+        $('.sidenav li').css({paddingRight:scrollw+'px'});
+    }
+}
+
+function init(){
+    floor_timer = setInterval( function(){
+        $('#floor-plans .carousel.carousel-slider').carousel('next');
+    }, 6000);
+    // $('#floor-plans').mouseover(function() {
+    //     clearInterval(floor_timer);
+    // });
+    // $('#floor-plans').mouseout(function() {
+    //     clearInterval(floor_timer);
+    //     $('#floor-plans .carousel.carousel-slider').carousel('next');
+    //     floor_timer = setInterval( function(){
+    //         $('#floor-plans .carousel.carousel-slider').carousel('next');
+    //     }, 2000);
+    // });
+
+    project_timer = setInterval( function(){
+        $('#project-images .carousel.carousel-slider').carousel('next');
+    }, 6000);
+    // $('#project-images').mouseover(function() {
+    //     clearInterval(project_timer);
+    // });
+    // $('#project-images').mouseout(function() {
+    //     clearInterval(project_timer);
+    //     $('#project-images .carousel.carousel-slider').carousel('next');
+    //     project_timer = setInterval( function(){
+    //         $('#project-images .carousel.carousel-slider').carousel('next');
+    //     }, 2000);
+    // });
+
+    checkScrollBars();
+}
